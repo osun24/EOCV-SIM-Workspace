61392a687355507906621085b143401b532ea384
osun24 <osun342@gmail.com>
2023-10-20 23:34:17 -0700

    add location

--- src/main/java/org/firstinspires/ftc/teamcode/BlueCubeDetectionPipeline.java
+++ src/main/java/org/firstinspires/ftc/teamcode/BlueCubeDetectionPipeline.java
@@ -1,54 +1,53 @@
-package org.firstinspires.ftc.teamcode;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.opencv.core.*;
-import org.opencv.imgproc.Imgproc;
-import java.util.List;
-import java.util.ArrayList;
-
-
 public class BlueCubeDetectionPipeline extends OpenCvPipeline {
+    private Telemetry telemetry;  // Add Telemetry object
     Mat hsv = new Mat();
     Mat mask = new Mat();
     Mat morphed = new Mat();
 
+    public BlueCubeDetectionPipeline(Telemetry telemetry) {  // Constructor to initialize telemetry
+        this.telemetry = telemetry;
+    }
+
     @Override
     public Mat processFrame(Mat input) {
-
-        // Convert the image from RGB to HSV
         Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
-
-        // Define the range for blue color
         Scalar lowerBlue = new Scalar(90, 100, 100);
         Scalar upperBlue = new Scalar(140, 255, 255);
-
-        // Threshold the image to get only blue colors
         Core.inRange(hsv, lowerBlue, upperBlue, mask);
-
-        // Use morphological operations to remove noise
+        
         Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
         Imgproc.morphologyEx(mask, morphed, Imgproc.MORPH_OPEN, kernel);
         Imgproc.morphologyEx(morphed, morphed, Imgproc.MORPH_CLOSE, kernel);
-
-        // Find contours
+        
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(morphed, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
         for (MatOfPoint contour : contours) {
             Rect rect = Imgproc.boundingRect(contour);
-            double aspectRatio = (double) rect.width / rect.height;
+            double centerX = rect.x + rect.width / 2.0;
+            double centerY = rect.y + rect.height / 2.0;
+
+            // Telemetry data for the center coordinates of each blue object
+            telemetry.addData("CenterX", centerX);
+            telemetry.addData("CenterY", centerY);
+
+            // Telemetry data for the area of each blue object
+            telemetry.addData("Area", Imgproc.contourArea(contour));
+
+            // adjust for location identification
+            if (centerX < input.cols() / 3) {
+                telemetry.addData("Position", "Left");
+            } else if (centerX < 2 * input.cols() / 3) {
+                telemetry.addData("Position", "Center");
+            } else {
+                telemetry.addData("Position", "Right");
+            }
+
             Imgproc.rectangle(input, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
-            /* 
-            if (aspectRatio > 0.8 && aspectRatio < 1.2 && rect.area() > 1000) { // Assuming cube contours will be roughly square
-                Imgproc.rectangle(input, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
-            }*/
         }
 
+        telemetry.update();  // Ensure you update telemetry after adding data
         return input;
     }
 }
