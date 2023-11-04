2915b72ee2826108be4c3470eea067ec235b3bf3
osun24 <114966159+osun24@users.noreply.github.com>
2023-11-04 13:10:01 -0700

    adjustments

--- src/main/java/org/firstinspires/ftc/teamcode/BlueCubeDetectionPipeline.java
+++ src/main/java/org/firstinspires/ftc/teamcode/BlueCubeDetectionPipeline.java
@@ -1,22 +1,27 @@
 package org.firstinspires.ftc.teamcode;
+import android.graphics.Canvas;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.*;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.opencv.imgproc.Imgproc;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BlueCubeDetectionPipeline extends OpenCvPipeline {
+// Currently only supports left camera - right camera may need to be added separately...
+public class BlueCubeDetectionPipeline implements VisionProcessor {
     private Telemetry telemetry;  // Add Telemetry object
     Mat hsv = new Mat();
     Mat mask = new Mat();
     Mat morphed = new Mat();
 
     // Create rectangle zones for the blue cubes
-    public float side_width = 198; 
-    public float side_height = 214;
+    public float side_width = 240;
+    public float side_height = 312;
     public float center_width = 300;
-    public float center_height = 204;
+    public float center_height = 330;
 
     // Set center coordinates for the blue cubes
     public float left_x = 23;
@@ -30,27 +35,39 @@ public class BlueCubeDetectionPipeline extends OpenCvPipeline {
     public Scalar lowerBlue = new Scalar(90, 72, 100);
     public Scalar upperBlue = new Scalar(140, 255, 255);
 
+    enum Detection {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    Detection detected;
+
     public BlueCubeDetectionPipeline(Telemetry telemetry) {  // Constructor to initialize telemetry
         this.telemetry = telemetry;
     }
 
     @Override
-    public Mat processFrame(Mat input) {
-        Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
+    public void init(int width, int height, CameraCalibration calibration) {
+
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_RGB2HSV);
         Core.inRange(hsv, lowerBlue, upperBlue, mask);
-        
+
         Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
         Imgproc.morphologyEx(mask, morphed, Imgproc.MORPH_OPEN, kernel);
         Imgproc.morphologyEx(morphed, morphed, Imgproc.MORPH_CLOSE, kernel);
-        
+
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(morphed, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
         // Draw rectangles around the zones in green
-        Imgproc.rectangle(input, new Point(center_x, center_y), new Point(center_x + center_width, center_y + center_height), new Scalar(0, 255, 0), 1);
-        Imgproc.rectangle(input, new Point(left_x, left_y), new Point(left_x + side_width, left_y + side_height), new Scalar(0,255, 0), 1);
-        Imgproc.rectangle(input, new Point(right_x, right_y), new Point(right_x + side_width, right_y + side_height), new Scalar(0,255, 0), 1);
+        Imgproc.rectangle(frame, new Point(center_x, center_y), new Point(center_x + center_width, center_y + center_height), new Scalar(0, 255, 0), 1);
+        Imgproc.rectangle(frame, new Point(left_x, left_y), new Point(left_x + side_width, left_y + side_height), new Scalar(0,255, 0), 1);
 
         for (MatOfPoint contour : contours) {
             Rect rect = Imgproc.boundingRect(contour);
@@ -59,31 +76,38 @@ public class BlueCubeDetectionPipeline extends OpenCvPipeline {
 
             double area = Imgproc.contourArea(contour);
 
-            // detect location 
+            // detect location
             if (area < 1000) {
                 break;
             }
 
             if (centerX > center_x && centerX < center_x + center_width && centerY > center_y && centerY < center_y + center_height) {
                 telemetry.addData("Location", "Center");
+                detected = Detection.CENTER;
             } else if (centerX > left_x && centerX < left_x + side_width && centerY > left_y && centerY < left_y + side_height) {
                 telemetry.addData("Location", "Left");
-            } else if (centerX > right_x && centerX < right_x + side_width && centerY > right_y && centerY < right_y + side_height) {
-                telemetry.addData("Location", "Right");
-            } else {
-                telemetry.addData("Location", "Unknown");
+                detected = Detection.LEFT;
+            } else if (detected == null) {
+                telemetry.addData("Location (Assumed)", "Right");
+                detected = Detection.RIGHT;
             }
 
             // Telemetry data for the area of each blue object
             telemetry.addData("Area", area);
 
-            // Telemetry data for the center coordinates of each blue object
-            telemetry.addData("CenterX, CenterY", centerX + ", " + centerY);  
-
-            Imgproc.rectangle(input, rect.tl(), rect.br(), new Scalar(255, 0, 0), 2);
+            Imgproc.rectangle(frame, rect.tl(), rect.br(), new Scalar(255, 0, 0), 2);
         }
 
-        telemetry.update();  // Ensure you update telemetry after adding data
-        return input;
+        telemetry.update();
+        return null;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+
+    }
+
+    public Detection getDetection() {
+        return detected;
     }
 }
